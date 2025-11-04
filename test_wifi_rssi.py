8c6911a4f700bfe31c315ca5c0e23c9c801a3a94
Aidan Leary <learyaidan@gmail.com>
2025-11-03 18:21:40 -0700

    restructure/improve wifi interface and add rssi monitoring class

---
+++ test_wifi_rssi.py
@@ -0,0 +1,30 @@
+from wifi_rssi import *
+from time import sleep
+
+def main(): 
+    card_name = "wlp0s20f3"
+    ch = 11
+
+    interface = WifiInterface(card_name)
+    monitor = WifiRssiMonitor(interface, ch)
+
+    period = 1
+    try:
+        while True: 
+            sleep(period)
+
+            rssi = monitor.get_max_rssi(period)
+            if rssi:
+                print(str(rssi))
+
+    except Exception as e: 
+        print("Exiting")
+        print(e)
+    finally:
+        monitor.stop()
+
+def print_rssi(rssi: float) -> None: 
+    print(rssi)
+
+if __name__ == "__main__": 
+    main()
---
+++ wifi_rssi.py
@@ -0,0 +1,185 @@
+'''
+For the monitoring of WiFi RSSI.
+'''
+from threading import Thread
+from typing import Optional
+from pyric import pyw 
+from scapy.all import sniff, RadioTap
+from subprocess import call
+from time import time
+from numpy import argmax, array
+
+
+class RssiTime: 
+    '''
+    RSSI with an associated time.
+    '''
+    rssi: float 
+    time: float 
+
+    def __init__(self, rssi: float, time: float): 
+        '''
+        Constructor.
+        '''
+        self.rssi = rssi 
+        self.time = time
+    def __str__(self):
+        '''
+        Converts to string.
+        '''
+        return f"rssi: {self.rssi}; time: {self.time}; "
+
+
+class WifiInterface:
+    '''
+    Class to interface with and control a WiFi card.
+    '''
+    _card_name: str
+    __card: pyw.Card
+    _channel: int
+
+    def __init__(self, card_name: str): 
+        '''
+        Contructor.
+
+        args: 
+            card_name: name of wireless card
+        '''
+        cards = pyw.winterfaces() 
+
+        if card_name in cards: 
+            self._card_name = card_name
+        elif f"{card_name}mon" in cards:
+            self._card_name = f"{card_name}mon"
+        else:
+            raise Exception("Given card not found.")
+
+        self.__card = pyw.getcard(self._card_name)
+
+    def start_monitor_mode(self, kill_processes: bool) -> None : 
+        '''
+        Enter monitor mode, calling a callback 
+        every time theres a new RSSI value.
+
+        args: 
+            kill_processes: kill conflicting processes
+        '''
+        if kill_processes:
+            call(["airmon-ng", "check", "kill"]) 
+
+        if not self._card_name.endswith("mon"):
+            print("Starting monitor mode.")
+
+            # this is the best way i've found to enter monitor mode
+            call(["airmon-ng", "start", self._card_name])
+            self._card_name += "mon"
+            self.__card = pyw.getcard(self._card_name)
+            print("Monitor mode started.")
+        else: 
+            print("Monitor mode already started.")
+
+    def stop_monitor_mode(self, start_network_manager: bool) -> None: 
+        '''
+        Stop monitor mode.
+
+        args: 
+            start_network_manager: start network manager (to reconnect to networks)
+        '''
+        print("Stopping monitor mode.")
+        call(["airmon-ng", "stop", self._card_name])
+        print("Monitor mode stopped.")
+
+        if start_network_manager:
+            call(["systemctl", "start", "NetworkManager"])
+
+    def set_channel(self, channel: int): 
+        '''
+        Set the channel of the WiFi card.
+        '''
+        pyw.chset(self.__card, channel)
+        self._channel = channel
+        print(f"WiFi channel set to {channel}.")
+
+
+class WifiRssiMonitor: 
+    '''
+    Class to monitor WiFi packets for Rssi.
+    '''
+    __wifi: WifiInterface
+    _rssi_list: list[float]
+    _time_list: list[float]
+    __monitor_thread: Thread
+    _seconds_of_data: int
+
+    def __init__(self, wifi: WifiInterface, ch: int): 
+        '''
+        Contructor. Starts monitor on given channel.
+        '''
+        self.__wifi = wifi
+        self.__wifi.start_monitor_mode(True)
+        self.__wifi.set_channel(ch)
+        self._rssi_list = []
+        self._time_list = []
+        self._seconds_of_data = 2
+
+        print("Starting packet capture.")
+        self.__monitor_thread = Thread(target=self.__monitor, daemon=True)
+        self.__monitor_thread.start()
+
+    def __monitor(self): 
+        '''
+        Run as background thread to monitor rssi.
+        '''
+        sniff(iface=self.__wifi._card_name, prn=self.__handle_packet, store=0)
+
+    def __handle_packet(self, pkt): 
+        '''
+        Handle a packet.
+        '''
+        current_time = time() 
+        try: 
+            rssi = float(pkt.getlayer(RadioTap).dBm_AntSignal)
+            self._rssi_list.append(rssi) 
+            self._time_list.append(current_time)
+        except AttributeError: 
+            pass
+
+    def get_max_rssi(self, n: float) -> Optional[RssiTime]:
+        '''
+        Get the max value over the last n seconds.
+
+        args: 
+            n: seconds to consider for max 
+
+        returns: RssiTime 
+        '''
+        current_time = time()
+
+        # remove elements that are too old
+        nsec_ago = current_time - self._seconds_of_data
+        elements_to_delete = 0
+        for t in self._time_list:
+            if (t < nsec_ago):
+                elements_to_delete += 1
+            else: 
+                break
+        del self._rssi_list[:elements_to_delete]
+        del self._time_list[:elements_to_delete]
+
+        # get max over last n seconds
+        nsec_ago = current_time - n
+        last_sec_times = [x for x in self._time_list if x > nsec_ago]
+        last_sec_rssi = self._rssi_list[len(self._rssi_list) - len(last_sec_times) :]
+
+        if len(last_sec_rssi) == 0: 
+            return None
+
+        idx = argmax(array(last_sec_rssi))
+        return RssiTime(last_sec_rssi[idx], last_sec_times[idx])
+
+    def stop(self):
+        '''
+        Stop monitoring.
+        '''
+        print("Stoping packet capture.") 
+        self.__wifi.stop_monitor_mode(False)
